c7cdf8efa6ad8826a92bc315e513e345c8bc4557
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-14 21:56:35 -0500

    Update

--- demos/src/components/Category.js
+++ demos/src/components/Category.js
@@ -1,5 +1,19 @@
+import { useEffect, useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 function Category(){
+
+
+let[category,setCategory]=useState([]);
+   
+
+    async function getCategory(){
+        let response = await fetch('https://dummyjson.com/products/category-list');
+        let show = await response.json();
+        setCategory(show);
+    }
+    useEffect(()=>{
+        getCategory();
+    },[]);
     return(
         <>
         </>
