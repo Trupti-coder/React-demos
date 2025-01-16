7cac851195c30a5fe1e85d401a4a79b6ee2c5428
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-15 22:06:57 -0500

    Update

--- demos/src/components/UserApi.js
+++ demos/src/components/UserApi.js
@@ -18,6 +18,78 @@ function UserApi(){
 
     return(
         <>
+         <div>
+      <h1>User Data</h1>
+
+      {/* Filters */}
+      <div>
+        <label>
+          Gender:
+          <select
+            value={genderFilter}
+            onChange={(e) => setGenderFilter(e.target.value)}
+          >
+            <option value="">All</option>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+          </select>
+        </label>
+
+        <label style={{ marginLeft: "20px" }}>
+          Blood Group:
+          <select
+            value={bloodGroupFilter}
+            onChange={(e) => setBloodGroupFilter(e.target.value)}
+          >
+            <option value="">All</option>
+            <option value="O+">O+</option>
+            <option value="A+">A+</option>
+            <option value="B+">B+</option>
+            <option value="AB+">AB+</option>
+            <option value="O-">O-</option>
+            <option value="A-">A-</option>
+            <option value="B-">B-</option>
+            <option value="AB-">AB-</option>
+          </select>
+        </label>
+      </div>
+
+      {/* Table */}
+      <table border="1" style={{ marginTop: "20px", width: "100%" }}>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Age</th>
+            <th>Email</th>
+            <th>Gender</th>
+            <th>Blood Group</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredUsers.length > 0 ? (
+            filteredUsers.map((user) => (
+              <tr key={user.id}>
+                <td>{user.id}</td>
+                <td>{user.firstName}</td>
+                <td>{user.lastName}</td>
+                <td>{user.age}</td>
+                <td>{user.email}</td>
+                <td>{user.gender}</td>
+                <td>{user.bloodGroup}</td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan="7" style={{ textAlign: "center" }}>
+                No users found.
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
         </>
     );
 }
