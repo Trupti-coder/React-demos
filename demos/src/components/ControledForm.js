233ff6ef1845b87c071a9abe174c66d1a104e1c7
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-15 21:49:22 -0500

    Update

--- demos/src/components/ControledForm.js
+++ demos/src/components/ControledForm.js
@@ -5,10 +5,85 @@ function ControledForm(){
     let[gender,setGender]=useState('');
     let[subject,setSubject]=useState([]);
     let[city,setCity]=useState("");
-    
+
     return(
         <>
         <form onSubmit={funSubmit}>
+
+        <div>
+                Name:<input type="text"
+                 onChange={(event)=>setName(event.target.value)} 
+                 />
+            </div>
+        
+            <div>
+                DOB:<input type="date"
+                 onChange={(event)=>setDob(event.target.value)}
+                  />
+            </div>
+            <div>
+                Gender:<input type="radio"
+                 name="gender" value="male" 
+                 onChange={(event)=>setGender(event.target.value)}
+                 />Male
+
+                <input type="radio"
+                 name="gender"
+                  value="female"
+                   onChange={(event)=>setGender(event.target.value)}
+                    />Female
+            </div>
+            <div>
+                City:<select onChange={(event)=>setCity(event.target.value)}>
+                    <option></option>
+                    <option>Pune</option>
+                    <option>Mumbai</option>
+                    <option>Delhi</option>
+                </select>
+            </div>
+            <div>
+                Subject:<input type="checkbox"
+                 value="Physics" 
+                 onChange={(e) => {
+                    const value = e.target.value;
+                    // Directly using setSubjects without unused expressions
+                    setSubject(prevSubjects => 
+                      e.target.checked 
+                        ? [...prevSubjects, value]  // Add if checked
+                        : prevSubjects.filter(subject => subject !== value)  // Remove if unchecked
+                    );
+                  }} 
+              
+                 />Physics<br />
+                <input type="checkbox" 
+                value="Chemistry"
+                onChange={(e) => {
+                    const value = e.target.value;
+                    // Directly using setSubjects without unused expressions
+                    setSubject(prevSubjects => 
+                      e.target.checked 
+                        ? [...prevSubjects, value]  //add if checked
+                        : prevSubjects.filter(subject => subject !== value)  // Remove if unchecked
+                    );
+                  }} 
+              
+                
+                />Chemistry <br />
+                <input type="checkbox"
+                 value="Mathematics"
+                 onChange={(e) => {
+                    const value = e.target.value;
+                    // Directly using setSubjects without unused expressions
+                    setSubject(prevSubjects => 
+                      e.target.checked 
+                        ? [...prevSubjects, value]  // Add if checked
+                        : prevSubjects.filter(subject => subject !== value)  // Remove if unchecked
+                    );
+                  }} 
+              
+                 />Mathematics
+            </div>
+            <button type="submit">Submit</button>
 
         </form>
         </>
