705936aa22e71828095664e563ba53d7eb3e6b27
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-16 21:54:01 -0500

    Update

--- demos/src/components/AddProduct.js
+++ demos/src/components/AddProduct.js
@@ -1,7 +1,17 @@
 function AddProduct(){
     return(
         <>
-        
+        <h3>Add product Form </h3>
+        <div>
+            ProductName:<input type="text" onChange={nameChange}/>
+        </div>
+        <div>
+            ProductPrice:<input type="text" onChange={priceChange}/>
+        </div>
+        <div>
+            <input type="button" value="AddProduct" onClick={funAdd}/>
+        </div>
+    </div>
         </>
     );
 }
