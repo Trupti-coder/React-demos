80483f415bbb6529ba337acd4dcec9b917e3324b
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-16 22:13:49 -0500

    Update ToDoList.js

--- demos/src/components/ToDoList.js
+++ demos/src/components/ToDoList.js
@@ -9,7 +9,17 @@ function ToDoList(){
         setTask(event.target.value);
           
       }
-      
+
+      function funAdd(){
+        if(task.trim() !==""){
+          setTasks([...tasks,task]);
+          setTask("");
+          console.log(tasks);
+        }
+    
+    
+      }
+
     return(
         <>
         <h3>ToDoList Application</h3>
