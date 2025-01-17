41a1aaf6e92b951a20f1f240cf0299943071e70d
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-16 22:10:43 -0500

    Update

--- demos/src/components/ToDoList.js
+++ demos/src/components/ToDoList.js
@@ -1,6 +1,10 @@
 function ToDoList(){
     return(
         <>
+        <h3>ToDoList Application</h3>
+    <input type="text"  value={task}  onChange={funTask}/>
+
+    <button onClick={funAdd}>Add</button>
         </>
     );
 }
