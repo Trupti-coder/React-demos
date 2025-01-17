4a1956223bed9d0e79616def6f397925c08494a6
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-16 21:52:59 -0500

    Create AddProduct.js

--- demos/src/App.js
+++ demos/src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
 import Category from './components/Category';
+import UserApi from './components/UserApi';
 
 function App() {
   return (
    <>
    <Category />
+   <hr></hr>
+   <UserApi />
    </>
   );
 }
---
+++ demos/src/components/AddProduct.js
@@ -0,0 +1,8 @@
+function AddProduct(){
+    return(
+        <>
+        
+        </>
+    );
+}
+export default AddProduct;
