5358100c232730a770874336fa4f002f61c0e746
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-17 20:04:46 -0500

    Update

--- demos/src/components/Counter.js
+++ demos/src/components/Counter.js
@@ -1,6 +1,10 @@
 function Counter(){
     return(
         <>
+         <h3>Count:{count}</h3>
+         <h3>Counter Example</h3>
+         <button onClick={funIncrement}>Increment</button>
+         <button onClick={funDecrement}>Decrement</button>
         </>
     );
 }
