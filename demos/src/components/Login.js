81eb8fcf2d1cf1c084904354c273843926c6c6ac
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-18 22:14:16 -0500

    Update Login.js

--- demos/src/components/Login.js
+++ demos/src/components/Login.js
@@ -1,7 +1,18 @@
 function Login(){
     return(
         <>
-        
+        <form>
+            <h3>Login</h3>
+            <div>
+                Email:<input type="email" required />
+            </div>
+            <div>
+                Password:<input type="password" required/>
+            </div>
+            <div>
+                <button type="submit">Login</button>
+            </div>
+        </form>
         </>
     );
 }
