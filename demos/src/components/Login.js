9e9e1c616e01c5b581881c90fc104091349045a6
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-18 22:21:15 -0500

    Update Login.js

--- demos/src/components/Login.js
+++ demos/src/components/Login.js
@@ -2,25 +2,27 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Login(){
-    let[email,setEmail]=useState();
-    let[password,setPassword]=useState();
+    let[email,setEmail]=useState('');
+    let[password,setPassword]=useState('');
+    let[error,setError]=useState();
+    
 
     return(
         <>
-        <form>
+        <form onSubmit={funSubmit}>
             <h3>Login</h3>
             <div>
-                Email:<input type="email" required />
+                Email:<input type="email" onChange={emailChange} required />
             </div>
             <div>
-                Password:<input type="password" required/>
+                Password:<input type="password" onChange={passwordChange} required/>
             </div>
             <div>
                 <button type="submit">Login</button>
             </div>
         </form>
         <div>
-            <p>If you do not have account<Link >signup here</Link> </p> 
+            <p>If you do not have account<Link to='/signup' >signup here</Link> </p> 
         </div>
         </>
     );
