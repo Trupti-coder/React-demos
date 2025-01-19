a880603d02579763fcbf2d819338b36a77e34b56
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-18 19:34:37 -0500

    Update SignUp.js

--- demos/src/components/SignUp.js
+++ demos/src/components/SignUp.js
@@ -1,7 +1,17 @@
 function SignUp(){
     return(
         <>
-        
+        <form>
+            <div>
+                Name:<input type="text" />
+            </div>
+            <div>
+                Email:<input type="email" required />
+            </div>
+            <div>
+                Password:<input type="password" required />
+            </div>
+        </form>
         </>
     );
 }
