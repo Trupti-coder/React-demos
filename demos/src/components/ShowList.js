864e67e8823ff63937d02957b157d0bd56ffbea1
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-19 16:32:48 -0500

    Update ShowList.js

--- demos/src/components/ShowList.js
+++ demos/src/components/ShowList.js
@@ -1,4 +1,15 @@
+import { useState } from "react";
+
 function ShowList(){
+
+    let[list,setList]=useState([]);
+    async function getAllList(){
+        let response=await fetch('https://dummyjson.com/recipes');
+        let result=await response.json();
+        console.log(result.recipes);
+        setList(result.recipes);
+    }
+
     return(
         <>
         </>
