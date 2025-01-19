a3faf4b3b3eeef528df61f7027a6f3e084a1a3f5
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-18 19:42:06 -0500

    Update SignUp.js

--- demos/src/components/SignUp.js
+++ demos/src/components/SignUp.js
@@ -1,17 +1,34 @@
+import { useState } from "react";
+
 function SignUp(){
+    let[name,setName]=useState("");
+    let[email,setEmail]=useState("");
+    let[password,setPassword]=useState("");
+
+    function nameChange(){
+
+    }
+
+    function emailChange(){
+
+    }
+
+    function passwordChange(){
+        
+    }
 
    
     return(
         <>
         <form>
             <div>
-                Name:<input type="text" />
+                Name:<input type="text" onChange={nameChange} />
             </div>
             <div>
-                Email:<input type="email" required />
+                Email:<input type="email" onChange={emailChange}  required />
             </div>
             <div>
-                Password:<input type="password" required />
+                Password:<input type="password" onChange={passwordChange} required />
             </div>
         </form>
         </>
