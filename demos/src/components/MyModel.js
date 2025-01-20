13a9f3dcf91955eebfcbfbb1e1de87085a414f17
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-20 18:04:16 -0500

    Update MyModel.js

--- demos/src/components/MyModel.js
+++ demos/src/components/MyModel.js
@@ -1,5 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-function MyModel(){
+import { useState } from 'react';
+function MyModel(props){
+
+    let[modelopen,setModelOpen]=useState(false);
+
+    let[txtId,setId]=useState();
+    let[txtName,setName]=useState();
+    let[txtIngredients,setIngredients]=useState();
+    let[txtInstruction,setInstruction]=useState();
+    let[txtprepTimeMinutes,setprepTimeMinutes]=useState();
 
     return(
         <>
